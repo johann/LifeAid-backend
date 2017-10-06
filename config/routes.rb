ed995600dfb9ffe1f5774e38d164303e2875497c
Johann Kerr <johannkerr@gmail.com>
2017-10-05 20:34:58 -0400

    removed api namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      post 'user_token' => 'user_token#create'
-      post 'users' => 'users#create'
-    end
-  end
+  # namespace :api do
+  #   namespace :v1 do
+  #
+  #   end
+  # end
+  post 'user_token' => 'user_token#create'
+  post 'users' => 'users#create'
   get 'hello', to: 'application#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
